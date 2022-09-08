734fe798ccf6736fb8020cc61a890024decebe2e
Jon Penton <hey@jondpenton.com>
2022-09-08 02:25:26 +0000

    feat: :sparkles: install all packages at once

--- install.sh
+++ install.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
 
+printenv
+
 brew update
 brew upgrade
 
-if [[ ! $(command -v carapace) ]]; then
-  echo 'Installing carapace...'
-  brew tap rsteube/homebrew-tap
-  brew install rsteube/tap/carapace
-fi
-
-if [[ ! $(command -v nu) ]]; then
-  # echo 'Reinstalling gcc...'
-  # brew reinstall gcc@11 # Broken dependency
-  echo 'Installing nu...'
-  # sudo apt-get install --yes gcc-multilib
-  # export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu"
-  brew install nushell
-  # brew reinstall nushell # Account for libgcc error
-fi
-
-if [[ ! $(command -v starship) ]]; then
-  echo 'Installing starship...'
-  brew install starship
-fi
+brew tap rsteube/homebrew-tap
+brew install rsteube/tap/carapace starship nushell
 
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}"
 
